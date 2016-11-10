9a929a83ecc8166dc47a6abef9e4dde03feabc60
cassthecoder <cassandra@matosmendez.com>
2016-11-10 09:37:24 -0700

    added gmail gem and created user mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
 
+	def user_mailer_preview
+		UserMailer.welcome_email(User.first)
+	end
+	
 end
